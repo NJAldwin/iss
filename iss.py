351f86ffa63cdb63237f5782f3c2cf6a00ec8be6
Nick Aldwin <Nick@Aldwin.us>
2012-03-20 19:04:15 -0400

    Add hexdump and class structures

--- iss.py
+++ iss.py
@@ -12,11 +12,53 @@
 
 # For usage, see README.md
 
-import sys, os
+import sys, os, binascii
 from struct import pack, unpack
 
 # Constants
 CODE =  0x00001000
+
+# Instructions
+class add:
+    def __init__(self, instr):
+        self.rs = slicebin(instr, 25, 21)
+        self.rt = slicebin(instr, 20, 16)
+        self.rd = slicebin(instr, 15, 11)
+        self.shamt = slicebin(instr, 10, 6)
+        self.funct = slicebin(instr, 5, 0)
+
+class addi:
+    def __init__(self, instr):
+        self.rs = slicebin(instr, 25, 21)
+        self.rt = slicebin(instr, 20, 16)
+        self.imm = slicebin(instr, 15, 0)
+
+class beq:
+    def __init__(self, instr):
+        self.rs = slicebin(instr, 25, 21)
+        self.rt = slicebin(instr, 20, 16)
+        self.addr = slicebin(instr, 15, 0)
+
+class lw:
+    def __init__(self, instr):
+        self.rs = slicebin(instr, 25, 21)
+        self.rt = slicebin(instr, 20, 16)
+        self.addr = slicebin(instr, 15, 0)
+
+class sw:
+    def __init__(self, instr):
+        self.rs = slicebin(instr, 25, 21)
+        self.rt = slicebin(instr, 20, 16)
+        self.addr = slicebin(instr, 15, 0)
+
+class j:
+    def __init__(self, instr):
+        self.target = slicebin(instr, 25, 0)
+
+
+def slicebin(i, high, low):
+    mask = 2L**(high - low + 1) -1
+    return (i >> low) & mask
 
 def usage():
     """ Prints program usage and exits """
@@ -35,6 +77,31 @@ def main():
         sys.exit("File does not exist: %s" % infile)
 
     print "input: %s" % infile
+
+    progfile = open(infile, 'rb')
+
+    # read memory
+    # todo
+
+    # read code
+    # todo
+
+    # print hexdump
+    # (will be removed later)
+    hexdump(progfile)
+
+    progfile.close()
+
+def hexdump(progfile):
+    print "Hexdump:"
+    progfile.seek(0)
+    while True:
+        pos = progfile.tell()
+        bytes = progfile.read(16)
+        if bytes == '':
+            break
+        fullbytes = unpack("<cccccccccccccccc", bytes)
+        print (hex(pos)[2:][:-1]).zfill(8) + '  ' + ' '.join(map(lambda b: binascii.hexlify(b), fullbytes))
 
 if __name__ == '__main__':
     main()
